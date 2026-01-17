b3386c1684dff19d2fbd63fa041d8ff9c96a75e4
Parminder Klair <perminder.klair@gmail.com>
2026-01-17 23:18:08 +0000

    Initial Commit

---
+++ WatchNotes Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WatchNotes Watch App
+//
+//  Created by Parminder Klair on 17/01/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WatchNotes Watch App/WatchNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchNotesApp.swift
+//  WatchNotes Watch App
+//
+//  Created by Parminder Klair on 17/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct WatchNotes_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
